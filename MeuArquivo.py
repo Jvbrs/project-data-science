69162ec617e68606fa77c36f3df26deb1f8b51b7
Your Name <joaovitorbastosborges@gmail.com>
2024-01-09 22:55:31 -0300

    first commit

---
+++ MeuArquivo.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+
+tabela_vendas = pd.read_excel('myvenv/Vendas.xlsx')
+
+pd.set_option('display.max_columns', None)
+print(tabela_vendas)
+
+faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
+
+quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+
+print('-' * 50)
+
+ticket_medio = (faturamento['Valor Final'] / quantidade ['Quantidade']).to_frame()
+print(ticket_medio)
+
